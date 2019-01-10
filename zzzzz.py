9a56d56de2d38e4c1ba2019ff26c0cbf52e9c291
wangxianrui <wangxianruii@163.com>
2019-01-10 16:57:11 +0800

    init

--- zzzzz.py
+++ zzzzz.py
@@ -1,7 +1,6 @@
-import torch
+import os
+import sys
 
-from model.mobilenetv2 import MobileNetV2Classify
-
-inputs = torch.rand([1, 3, 224, 224])
-model = MobileNetV2Classify(10)
-print(model(inputs))
+dir_list = os.listdir('checkpoint')
+dir_list = sorted(dir_list,  key=lambda x: os.path.getmtime(os.path.join('checkpoint', x)))
+print(dir_list)
